3226793f84b9fe3ed3405667a2ddf27180505176
Shaurya Pratap Singh <shauryasinghad01@gmail.com>
2024-05-24 01:02:35 +0530

    adding to git for hosting on render.io and vercel

---
+++ server.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const router = express.Router();
+const cors = require("cors");
+const nodemailer = require("nodemailer");
+require("dotenv").config();
+
+// server used to send send emails
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use("/", router);
+
+// console.log(process.env.EMAIL_USER);
+// console.log(process.env.EMAIL_PASS);
+
+const contactEmail = nodemailer.createTransport({
+  service: "gmail",
+  secure: true,
+  port: 465,
+  auth: {
+    user: process.env.AUTHUSER,
+    pass: process.env.AUTHPASS,
+  },
+});
+
+contactEmail.verify((error) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Ready to Send");
+  }
+});
+
+router.post("/contact", (req, res) => {
+  const name = req.body.firstName + req.body.lastName;
+  const email = req.body.email;
+  const message = req.body.message;
+  const phone = req.body.phone;
+  const receiverMail = {
+    from: name,
+    to: process.env.RECIEVER_EMAIL,
+    subject: "Contact Form Submission - Portfolio",
+    html: `<p>Name: ${name}</p>
+           <p>Email: ${email}</p>
+           <p>Phone: ${phone}</p>
+           <p>Message: ${message}</p>`,
+  };
+  contactEmail.sendMail(receiverMail, (error) => {
+    if (error) {
+      res.json(error);
+    } else {
+      res.json({ code: 200, status: "Message Sent" });
+    }
+  });
+});
+
+app.listen(process.env.PORT, () =>
+  console.log(`Server Running at port ${process.env.PORT}`)
+);
