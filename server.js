9fc8687f9c9a9c45dfe09da8b40216ea9c6049d8
Shaurya Pratap Singh <shauryasinghad01@gmail.com>
2024-05-27 01:23:28 +0530

    used cors for backend and frontend to get connected working on two different ports

--- server.js
+++ server.js
@@ -6,7 +6,13 @@ require("dotenv").config();
 
 // server used to send send emails
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use("/", router);
 
